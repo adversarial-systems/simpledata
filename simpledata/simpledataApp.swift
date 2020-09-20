44c6685389f6c31b7bfbe07a45beedd433bbc502
JB Smith <jbsmith@adversarial.systems>
2020-09-20 04:54:51 -0700

    Initial Commit

---
+++ simpledata/simpledataApp.swift
@@ -0,0 +1,17 @@
+//
+//  simpledataApp.swift
+//  simpledata
+//
+//  Created by user on 9/20/20.
+//
+
+import SwiftUI
+
+@main
+struct simpledataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
